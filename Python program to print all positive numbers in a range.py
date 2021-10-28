141b9c2a8e10e477734d1a1dec1360bcd600da9c
RexX <87025636+Rishi-2607@users.noreply.github.com>
2021-10-28 19:41:17 +0530

    Create Python program to print all positive numbers in a range.py

---
+++ Python program to print all positive numbers in a range.py
@@ -0,0 +1,9 @@
+start = int(input("Enter the start of range: "))
+end = int(input("Enter the end of range: "))
+  
+# iterating each number in list
+for num in range(start, end + 1):
+      
+    # checking condition
+    if num >= 0:
+        print(num, end = " ")
